759124357adfe06207eb75f457186a55d18b01cd
vijaychandar1989 <vijaychandar1989@gmail.com>
2015-08-18 00:21:05 +0530

    zz
    zz

---
+++ SampleApp/SampleApp/SampleApp.Shared/ViewModel/CustomerViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SampleApp.Model;
+
+namespace SampleApp.ViewModel
+{
+    class CustomerViewModel
+    {
+        private int _Id;
+        public int Id
+        {
+            get { return _Id; }
+            set { if(}
+        }
+
+        public CustomerViewModel()
+        { }
+
+
+        public IEnumerable<Customer> GetCustomerById(int CustomerId)
+        {
+            return new IEnumerable<Customer>();
+        }
+
+    }
+}
